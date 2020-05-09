8335a241eb8912da71c05d4c8c2e17df76b19ff3
Gökay <48670495+gokay-urenc@users.noreply.github.com>
2020-05-09 23:01:49 +0300

    Initial commit

---
+++ Switch_Case_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Case_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Yetki giriniz: ");
+            string yetki = Console.ReadLine();
+            switch (yetki)
+            {
+                case "Admin":
+                case "Yönetici":
+                case "Başkan":
+                case "Ceo":
+                    Console.Write("Yönetim paneline yönlendiriliyorsunuz.");
+                    break;
+                case "Üye":
+                    Console.Write("Ana sayfaya yönlendiriliyorsunuz.");
+                    break;
+                default:
+                    Console.Write("Bu sayfayı görmek için yetkiniz yok.");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
+// Kullanıcıdan yetkisinin ne olduğunu öğrenen ve ona göre yönlendirme yapan program. Yetkiler: admin, yönetici, başkan, ceo. Bunlar yönetim paneline        yönlendirilecek. Üye ana sayfaya yönlendirilecek. Geçerli yetki yoksa uyarı verilecek.
